22adb9d7c7561bb75da705f4b233fbc383152b6f
Kynake Risk <kynakerisk@gmail.com>
2025-09-08 03:00:16 -0300

    Add new StringLiterals utils class

---
+++ src/main/java/com/mitchej123/hodgepodge/util/StringLiterals.java
@@ -0,0 +1,7 @@
+package com.mitchej123.hodgepodge.util;
+
+public class StringLiterals {
+
+    public static final String EXU_SPIKE_REPAIR_COST_KEY = "hodgepodge_RepairCost";
+    public static final String EXU_SPIKE_OVERCHANT_KEY = "hodgepodge_overchanted";
+}
